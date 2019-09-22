4f434ce342e15abd35b3970d1c8fd64ad679e5e6
martin <martin.bagshaw@redsift.io>
2019-09-22 16:36:20 +0100

    Set up general theme file #42

---
+++ styles/globalStyle.js
@@ -0,0 +1,43 @@
+// red: rgb(240, 83, 88)
+// orange: rgb(249, 158, 81)
+// purple: rgb(53, 52, 143)
+// midGrey: rgb(147, 149, 152)
+// grey: rgb(88, 89, 91)
+const colors = {
+  black: '#000',
+  white: '#fff',
+
+  midGrey: '#939598',
+  grey: '#58595b',
+
+  red: '#f05358',
+  orange: '#f89e51',
+  purple: '#35348f'
+
+};
+
+const fontFamily = {
+  main: '\'Domus\', sans-serif',
+  sub: '\'Sofia Pro\', sans-serif',
+  code: '\'Source Code Pro\', monospace'
+};
+
+const font = {
+  Xsmall: '0.8125rem',
+  small: '1rem',
+  med: '1.25rem',
+  large: '1.5rem',
+  Xlarge: '2rem',
+  massive: '3rem'
+};
+
+const breakpoint = {
+  Xsmall: '480px',
+  small: '600px',
+  tablet: '768px',
+  large: '1024px',
+  Xlarge: '1280px',
+  massive: '1440px'
+};
+
+export { colors, fontFamily, font, breakpoint };
