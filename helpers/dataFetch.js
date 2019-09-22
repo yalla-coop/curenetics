33838e601b09dee13381ebf59388f75a59957119
mr-bagglesworth <martinbagshaw1@gmail.com>
2019-09-22 22:20:48 +0100

    initial data filter functions

--- helpers/dataFetch.js
+++ helpers/dataFetch.js
@@ -1,19 +1,43 @@
 const fetch = require("node-fetch");
 const sample = 'https://raw.githubusercontent.com/susanX/datatest/master/uk-Y018-2000-m-70.json';
 
-const { getTrialsByKey } = require("./filterFunctions");
+const {
+  getTrialsByKey,
+  getValidTrialsByKey,
+  getTrialsByValue
+} = require("./filterFunctions");
 
+
+
+// run this with `node dataFetch.js`
 const fetchFunc = url => {
   fetch(url)
     .then(result => result.json())
     .then(result => {
-      console.log('result count is: ', result.size);
+      console.log('total result count is: ', result.size);
       const resultArray = result.results;
       
       // do some filtering here
       // > log out the count to reduce noise
-      console.log('no. of trials with data for Phase: ', getTrialsByKey('Phase', resultArray).length);
 
+      // 1.
+      console.log('no. of trials with data for Phase: ', getTrialsByKey('Phase', resultArray).length);
+      console.log('no. of trials with Keywords (property): ', getTrialsByKey('Keywords', resultArray).length);
+
+      // 2.
+      console.log('no. of trials with valid Keywords: ', getValidTrialsByKey('Keywords', resultArray).length);
+      console.log('no. of trials with valid minimum age: ', getValidTrialsByKey('MinAge', resultArray).length);
+      console.log('no. of trials with valid Phase: ', getValidTrialsByKey('Phase', resultArray).length);
+
+      // 3.
+      console.log('no. of trials of Phase 1: ', getTrialsByValue('Phase', resultArray, 1 ).length);
+      console.log('no. of trials of Phase 2: ', getTrialsByValue('Phase', resultArray, 2 ).length);
+      console.log('no. of trials of Phase 3: ', getTrialsByValue('Phase', resultArray, 3 ).length);
+      console.log('no. of trials that are recruiting: ', getTrialsByValue('OverallStatus', resultArray, 'Recruiting' ).length);
+      console.log('no. of trials that are active, but not recruiting: ', getTrialsByValue('OverallStatus', resultArray, 'Active, not recruiting' ).length);
+      console.log('no. of trials that are completed: ', getTrialsByValue('OverallStatus', resultArray, 'Completed' ).length);
+      
+      
 
 
     })
