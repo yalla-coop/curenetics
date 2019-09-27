ba902f0358fe610190178b6bb312d108f23c1158
mr-bagglesworth <martinbagshaw1@gmail.com>
2019-09-27 01:37:39 +0100

    add fonts

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/reset.css';
 
 // icon testing
 // svgs downloaded from: https://material.io/resources/icons
@@ -8,7 +9,35 @@ import Calendar from './common/icons/Calendar';
 import Tick from './common/icons/Tick';
 
 import styled from 'styled-components';
-import { breakpoint, colors } from '../styles/globalStyles'
+import { breakpoint, colors, fontFamily, font } from '../styles/globalStyles'
+
+
+const Main = styled.main`
+  @import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
+  @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+  font-family: ${fontFamily.body};
+`;
+
+const Title = styled.h1`
+  font-size: ${font.massive};
+  font-family: ${fontFamily.main};
+  font-weight: 600;
+  margin-bottom: 2rem;
+`;
+
+const Paragraph = styled.p`
+  font-weight: 400;
+  font-size: ${font.small};
+  @media only screen and (min-width: ${breakpoint.Xsmall}) {
+    font-size: ${font.med};
+  }
+`;
+
+const Container = styled.section`
+  padding: 1rem;
+  margin: 0 auto;
+  max-width: ${breakpoint.massive};
+`;
 
 const IconContainer = styled.div`
   display: flex;
@@ -21,8 +50,12 @@ const IconContainer = styled.div`
 
 function App() {
   return (
-   <div>
-     And so it begins!!!!!!
+   <Main>
+     <Container>
+      <Title>And so it begins!!!!!!</Title>
+      <Paragraph>Here is a paragraph with Roboto font</Paragraph>
+     </Container>
+
      <IconContainer>
       <Arrow />
       <Arrow width={60} fill={colors.confirm}/>
@@ -33,7 +66,8 @@ function App() {
       <Calendar/>
       <Calendar width={75} fill={colors.accent}/>
      </IconContainer>
-   </div>
+
+   </Main>
   );
 }
 
