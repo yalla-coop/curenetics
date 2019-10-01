7311ffbb67b9eaf15a0076a3627ff46ab46e98ce
MohammedYehia <m-ya@windowslive.com>
2019-10-02 02:40:44 +0300

    create the main component and add the first half of the page #27

---
+++ src/components/pages/trialDetail/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from '../../common/Layout';
+
+import TrialDetailHeader from './TrialDetailHeader';
+
+const TrialDetail = () => (
+  <Container isCenter={true}>
+    <TrialDetailHeader />
+  </Container>
+);
+
+export default TrialDetail;
