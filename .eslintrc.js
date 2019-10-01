482b6a165951d6484d77743ff0cdabb8d48f7bb7
MohammedYehia <m-ya@windowslive.com>
2019-10-01 21:47:51 +0300

    configure linter for tabs instead of spaces & add iqnore for js/jsx extension #15

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,19 @@
 module.exports = {
-    "extends":["airbnb-base", "plugin:react/recommended"],
-    "parser": "babel-eslint",
-    "env":{ 
-      "browser": true
-    }
-  };
+	extends: ["airbnb-base", "plugin:react/recommended"],
+	parser: "babel-eslint",
+	env: {
+		browser: true
+	},
+	rules: {
+		indent: [2, "tab"],
+		"no-tabs": 0,
+	},
+	"settings": {
+		// iqnore import that not include js/jsx file extension
+		"import/resolver": {
+			"node": {
+				"extensions": [".js", ".jsx"]
+			}
+		}
+	},
+};
