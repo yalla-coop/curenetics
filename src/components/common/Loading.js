60b70380ec2873e33fc5ca4d4c4933c701fb8469
susanX <susan.gooding@yahoo.co.uk>
2019-10-06 22:03:59 +0100

    Loading, +const -breakingpoint

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -2,24 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 import Spinner from './icons/Spinner';
 import { breakpoint } from '../../styles/globalStyles';
-import { Header, Container } from '../common/Layout';
-import { Title, Paragraph } from '../common/Typography';
+import { Header, Container } from './Layout';
+import { Title, Paragraph } from './Typography';
 
 const Spacer = styled.div`
   padding-top: 6rem;
 `;
 
-let title1 = 'Processing....';
-let para1a = 'We are currently analysing the data to extract the relevant medical entities.';
-let para1b = 'This can take a few minutes so please do not refresh this page.';
+const title1 = 'Processing....';
+const para1a = 'We are currently analysing the data to extract the relevant medical entities.';
+const para1b = 'This can take a few minutes so please do not refresh this page.';
 
-let title2 = 'Finding Clinical Trials....';
-let para2a = 'We are currently filtering Clinical Trials across the UK to find matches for each of the entries provided.';
-let para2b = 'This can take a few minutes so please do not refresh this page.';
+const title2 = 'Finding Clinical Trials....';
+const para2a = 'We are currently filtering Clinical Trials across the UK to find matches for each of the entries provided.';
+const para2b = 'This can take a few minutes so please do not refresh this page.';
 
 const Loading = () => (
   <>  
-      <Header><Spacer />
+      <Header isCenter><Spacer />
       <Spinner />
       <Title>{title1}</Title>
       <Paragraph>{para1a}</Paragraph>
