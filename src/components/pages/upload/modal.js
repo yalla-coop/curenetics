ce4f876d7d162c1c3ced26ad7d6e679dc40fd97e
wright1 <prosperwright@yahoo.co.uk>
2019-10-23 09:24:52 +0100

    modal changes for IE view

--- src/components/pages/upload/modal.js
+++ src/components/pages/upload/modal.js
@@ -12,7 +12,7 @@ const Bkground = styled.section`
   z-index: 1;
   background-color: rgba(0, 0, 0, 0.4);
   div {
-    width: 45vw;
+    width: 65vw;
     height: auto;
     margin: -10vh auto;
     padding: 2vw;
@@ -27,36 +27,41 @@ const Bkground = styled.section`
 const Head = styled.section`
   display: flex;
   flex-direction: column;
-  padding: 1vw;
+  padding: 2vw;
   justify-items: center;
   align-items: center;
   margin-top: -3vh;
+`;
+const Img = styled.img`
+  padding-bottom: 2vh;
 `;
 
 const Modual = ({ onClick }) => (
   <Bkground>
     <div>
       <Head>
-        <img src={exclamation} alt="exclamation mark" />
+        <Img src={exclamation} alt="exclamation mark" />
         <SubHeading>
           This website uses local storage and remote data analysis
         </SubHeading>
       </Head>
-      <Paragraph>
+      <Paragraph isLight>
         Medical professionals must only used this app on a secure or
         trust-approved computer. The uploaded files(s) will be stored locally
         but wiped when you close the app.
       </Paragraph>
-      <Paragraph>
+      <Paragraph isLight>
         Medical information extracted is immediately pseudo-anonymised and then
         encrypted before analyses. However, please do not use any sensitive
         information within the actual filename.
       </Paragraph>
-      <Paragraph>
+      <Paragraph isLight>
         This current software iteration is only optimised for scientific and
         research purposes. It must not serve as aid in clinical decision making.
       </Paragraph>
-      <Paragraph>By using this app you consent to this data use.</Paragraph>
+      <Paragraph isLight>
+        By using this app you consent to this data use.
+      </Paragraph>
       <Head>
         <OrangeButton onClick={e => onClick(e)}>OK</OrangeButton>
       </Head>
