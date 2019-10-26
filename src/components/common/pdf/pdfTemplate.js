698c2c77b4c7cff776c54221c0c1465a650caeb6
MohammedYehia <m-ya@windowslive.com>
2019-10-26 03:16:50 +0300

    remove old commented code

--- src/components/common/pdf/pdfTemplate.js
+++ src/components/common/pdf/pdfTemplate.js
@@ -2,27 +2,7 @@ import React from 'react';
 import { Document, Page } from '@react-pdf/renderer';
 import { styles } from './pdfStyles';
 import { isPotential, filterByEligibility } from '../../../helpers/eligibility';
-// import Roboto from '../fonts/Roboto-Regular.ttf';
-// import RobotoBold from '../fonts/Roboto-Bold.ttf';
-
-// import templates for single trial and mulitple trials here:
 import SingleTrial from './singleTrial';
-
-// Font.register({
-//   family: 'Roboto',
-//   src: Roboto,
-// });
-
-// Font.register({
-//   family: 'Roboto-bold',
-//   src: RobotoBold,
-// });
-
-// data = result data
-// isPotential = green or orange colour
-// type = the type of pdf template to use. Single trial by default
-
-// const PdfTemplate = ({ data, patientsInfo, type = 'single' }) => {
 
 class PdfTemplate extends React.Component {
   shouldComponentUpdate(nextProps) {
